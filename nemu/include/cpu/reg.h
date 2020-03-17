2599daf3078d27743547f60a8403d8275bdc3d71
tracer-ics2017 <tracer@njuics.org>
2020-03-17 15:21:50 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:21:50 up  1:32,  2 users,  load average: 0.27, 0.13, 0.10 a147c916f4995d4faa4a49b7a10cb604a9f4dd

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -22,7 +22,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  *有关寄存器编码方案的更多详细信息，请参见i386手册。
  */
 
-typedef struct {
+typedef union {
   union{
     uint32_t _32;
     uint16_t _16;
@@ -43,7 +43,6 @@ typedef struct {
   struct
   {
     rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
     vaddr_t eip;
   };
 } CPU_state;
