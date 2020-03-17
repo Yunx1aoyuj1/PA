e304e4e8453afec8c7cdea731d42e9f63657d31e
tracer-ics2017 <tracer@njuics.org>
2020-03-17 19:57:30 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:57:30 up  1:37,  2 users,  load average: 0.21, 0.12, 0.13 b7a5999af1d872e2ac7d8f0cfa046ba6483d47a7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -113,7 +113,7 @@ static int cmd_x(char *args){
   for(i = 0;i < times; i ++){
     data = vaddr_read(addr,4);    //如何调用，怎么传递参数，请阅读代码
     //每次循环将读取到的数据用 printf 打印出来
-    printf("0x%x 0x%8x ... %d\n",addr,data,data);//如果你不知道应该打印什么，可以参考参考输出形式
+    printf("0x%08x 0x%08x ... %d\n",addr,data,data);//如果你不知道应该打印什么，可以参考参考输出形式
     addr += 4;
   }
   return 0;
