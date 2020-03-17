2384cc00f2c3dc856e365138de9600273f596a92
tracer-ics2017 <tracer@njuics.org>
2020-03-17 15:58:13 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:58:13 up  2:09,  2 users,  load average: 0.18, 0.23, 0.12 ea4325261c25dfdd70266410a881508aa65c6fee

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -28,14 +28,13 @@ typedef union {
     uint16_t _16;
     uint8_t _8[2];
   } gpr[8];
+  //it is a 32-bit gpr 
 
   /* Do NOT change the order of the GPRs' definitions. */
-
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
   /*请勿更改GPR定义的顺序。 */
-
   /* 
     在NEMU中，rtlreg_t恰好是uint32_t。 这使得RTL指令
     在PA2中能够直接访问这些寄存器。
