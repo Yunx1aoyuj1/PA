204d9f1ab917cbfd05eb9c50d13f06c9c5b942ba
tracer-ics2017 <tracer@njuics.org>
2020-03-17 19:47:16 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:47:16 up  1:26,  2 users,  load average: 0.12, 0.18, 0.17 6f694891e1eb8064d8c07424b6af88481aecfff

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -44,17 +44,14 @@ static int cmd_si(char *args){
   // TODO: 利用 strtok 读取出 N
   char *token = strtok(args," ");
   int step = 0;//initialize
-  if(token == NULL)
-  {
+  if(token == NULL){
     step = 1;
   }
-  else
-  {
+  else{
   int number,weight,length;//length of token
     length = strlen(token);
     int i;
-    for (weight = 1,i = 0; i < length ; weight *= 10, i ++)//change string into integer
-    {
+    for (weight = 1,i = 0; i < length ; weight *= 10, i ++){//change string into integer
       number = token[i] - '0';
       step += number * weight;
     }
@@ -67,9 +64,9 @@ static int cmd_si(char *args){
 static int cmd_info(char *args){
   // 分割字符
   char *token = strtok(args," ");
+
   // 判断子命令是否是r
-  if (strcmp(token,"r") == 0)
-  {
+  if (strcmp(token,"r") == 0){
       // 依次打印所有寄存器
       // using for 
       // 这里给个例子：打印出 eax 寄存器的值
@@ -78,9 +75,46 @@ static int cmd_info(char *args){
         printf("%s:\t 0x%8x\t  %d\n", regsl[i], cpu.gpr[i]._32 , cpu.gpr[i]._32);
       
   }
-  else if (strcmp(token,"w") == 0)
-  {
+  else if (strcmp(token,"w") == 0){
       // 这里我们会在 PA1.3 中实现
+  }
+  return 0;
+}
+
+static int cmd_x(char *args){
+  //分割字符串，得到起始位置和要读取的次数
+  char *token = strtok(args," ");
+  uint32_t addr = 0;
+  int times = 0,weight = 1;
+  int i,number;
+  //times
+  for (weight = 1,i = 0; i < strlen(token) ; weight *= 10, i ++){//change string into integer
+      number = token[i] - '0';
+      times += number * weight;
+  }
+
+  //address
+  token = strtok(args," ");
+  for (weight = 1,i = strlen(token) -1; i > 1; i --,weight *= 16){
+    token[i] = toupper(token[i]);
+    if(token[i] <='9' && token[i] >= '0'){
+      number = token[i] - '0';
+      addr += number * weight;
+    }
+    else{
+      number = token[i] - 'A';
+      addr += number * weight;
+    }  
+  }
+  
+  printf("Address\tDword block\t ... Byte sequence");
+  //循环使用 vaddr_read 函数来读取内存
+  uint32_t data;
+  for(i = 0;i < times; i ++){
+    data = vaddr_read(addr,4);    //如何调用，怎么传递参数，请阅读代码
+    //每次循环将读取到的数据用 printf 打印出来
+    printf("0x%x 0x%8x ... %d\n",addr,data,data);//如果你不知道应该打印什么，可以参考参考输出形式
+    addr += 4;
   }
   return 0;
 }
@@ -96,7 +130,8 @@ static struct {
 
   /* TODO: Add more commands */
   {"si","Single step. si x ,time x", cmd_si},
-  {"info","\t -r Print each register information \t -w ",cmd_info},
+  {"info","\t -r Print each register information \t -w ",cmd_info},//not finish 
+  {"x","\t {times} {address}  Scan memory",cmd_x},
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
