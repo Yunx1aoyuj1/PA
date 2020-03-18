38ebd05305d82163668d65114c235cdf73cf0265
tracer-ics2017 <tracer@njuics.org>
2020-03-18 23:40:53 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:40:53 up 7 min,  2 users,  load average: 0.47, 0.33, 0.16 2ebc4d6f962df66031a0c8b2e478cf7687a17

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -88,12 +88,11 @@ static int cmd_x(char *args){
   int times = 0,weight = 1;
   int i,number;
   //times
-  printf("%s",token);
   for (weight = 1,i = 0; i < strlen(token) ; weight *= 10, i ++){//change string into integer
       number = token[i] - '0';
       times += number * weight;
   }
-
+  printf("%d\n",times);
   //address
   token = strtok(NULL ," ");
   for (weight = 1,i = strlen(token) -1; i > 1; i --,weight *= 16){
