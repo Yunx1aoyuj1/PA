6bacaf3f5dc19d67e39ad72428e86dbfb13cb006
tracer-ics2017 <tracer@njuics.org>
2020-04-10 22:51:32 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-46-generic #38~18.04.1-Ubuntu SMP Tue Mar 31 04:17:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:51:32 up  3:38,  2 users,  load average: 0.37, 0.28, 0.27 b5a08a12e9091f3e5a5cc877ec7a5f02142087ba

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -145,9 +145,7 @@ uint32_t eval(int p,int q) {
     }
 
     if (p > q) {
-        printf("%d,%d\n",p,q);
-        printf("Bad expression\n");
-        assert(0);
+        return 0;
     }
 
     else if (p == q) {
