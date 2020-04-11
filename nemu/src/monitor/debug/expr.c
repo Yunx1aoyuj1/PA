183318f89923be588f85d52188c23e3d13f12ea9
tracer-ics2017 <tracer@njuics.org>
2020-04-11 22:13:18 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-46-generic #38~18.04.1-Ubuntu SMP Tue Mar 31 04:17:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:13:18 up  2:27,  2 users,  load average: 0.50, 0.30, 0.14 cc34081ced06161d40a59efc15496a9d5351c7f0

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -115,7 +115,6 @@ int check_parentheses(int p, int q){
     ")" weights -1;
   */
   for ( ; i <= q; i++){
-    printf("\n%d,%d,%d\n",number_of_bracket,p,q);
     if(tokens[i].type == '(' )
       number_of_bracket ++;
     else if( tokens[i].type == ')')
@@ -125,7 +124,6 @@ int check_parentheses(int p, int q){
     }
       
   }
-  printf("\naxspkd::: %d,%d,%d,%d\n",number_of_bracket,if_surrounded,p,q);
   if(number_of_bracket) return 0;//false //an wrong expression
   else if(!number_of_bracket && if_surrounded ) return 1;//true
   else if(!number_of_bracket && !if_surrounded ) return -1;
@@ -161,6 +159,7 @@ uint32_t find_dominated_op(int p, int q){
         }
       }
     }
+    printf("\n %d,%d,%d,%d,%d\n",number_of_bracket,op,p,q,i);
   }
   return op;
 }
