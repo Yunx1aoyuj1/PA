1742e48661ae32acfaee86e3070d40393aea378b
tracer-ics2017 <tracer@njuics.org>
2020-04-11 17:31:46 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-46-generic #38~18.04.1-Ubuntu SMP Tue Mar 31 04:17:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:31:46 up  2:31,  2 users,  load average: 0.29, 0.30, 0.27 c6c1966bc137a9037766cc3d326fb5557a985be9

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -122,9 +122,9 @@ void list_watchpoint(void){     //显示当前在使用状态中的监视点列�
     printf("No watchpoint!\n");
     return ;
   }
-  printf("NO Expr\t\tOld Value");
+  printf("NO Expr\t\tOld Value\n");
   for ( ; wp;  wp = wp -> next){
-    printf("%d %s\t\t0x%08X",wp -> NO,wp -> expr ,wp -> old_val);
+    printf("%d %s\t\t0x%08X \n",wp -> NO,wp -> expr ,wp -> old_val);
   }
 }
 
