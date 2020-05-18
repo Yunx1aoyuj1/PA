742d41cfe378329052916c2c40489855b94a5fd7
tracer-ics2017 <tracer@njuics.org>
2020-05-18 11:03:04 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:03:04 up  1:03,  2 users,  load average: 0.49, 0.29, 0.16 29629afab24c77f85d7cb80aaf24b3da5b8d982

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -39,8 +39,18 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   //TODO();
-  int simm = instr_fetch(eip,op -> width);
-  op -> simm = simm ; 
+  if(op -> width == 1){
+    t0 =   (uint16_t) instr_fetch(eip,op -> width);
+    rtl_sext(&t1,&t0,1);
+  }
+  else if(op -> width == 2){
+    t0 =   (uint16_t) instr_fetch(eip,op -> width);
+    rtl_sext(&t1,&t0,2);
+  }
+  else if(op -> width == 4){
+    t1 =   instr_fetch(eip,op -> width);
+  }
+  op -> simm = t1 ; 
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
