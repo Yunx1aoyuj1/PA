094f05a2e55f4d3e3d982c65fbca215dbea5498c
tracer-ics2017 <tracer@njuics.org>
2020-05-18 11:40:35 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:40:35 up  1:41,  2 users,  load average: 1.25, 0.53, 0.31 a21d27db79039c8ade3f19f23aa52dc71226d4e2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -141,8 +141,9 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
   int32_t help = (int32_t)(*src1);
-  t0 = (help) << (32 - 8*width);
-  t0 = t0 >> (32 - 8*width);
+  help = (help) << (32 - 8*width);
+  help = help >> (32 - 8*width);
+  t0 = help;
   printf("\n\n1:0x%x\n",t0);
   *dest = t0;
 }
