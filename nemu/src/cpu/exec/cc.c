e5a0b330c068d60faf0aa1ec3592d19d808d570b
tracer-ics2017 <tracer@njuics.org>
2020-05-18 15:03:09 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:03:09 up  1:33,  2 users,  load average: 0.23, 0.14, 0.06 c4c00ce416865a1856fd59677c897e01d090462

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,18 +14,39 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_O:{
+      rtl_get_OF(&t0);
+    }break;
+    case CC_B:{
+      rtl_get_CF(&t0);
+    }break;
+    case CC_E:{
+      rtl_get_ZF(&t0);
+    }break;
+    case CC_BE:{
+      rtl_get_ZF(&t0);
+      rtl_get_CF(&t1);
+      rtl_or(&t0,&t0,&t1);
+    }break;
+    case CC_S:{
+      rtl_get_SF(&t0);
+    }break;
+    case CC_L:{
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(&t0,&t0,&t1);
+    }break;
+    case CC_LE:{
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_get_ZF(&t2);
+      rtl_xor(&t0,&t0,&t1);
+      rtl_or(&t0,&t0,&t2);
+    }break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
-
+  *dest = t0; 
   if (invert) {
     rtl_xori(dest, dest, 0x1);
   }
