61b40152ef61de63f961fa4d7a4b5df6216518bd
tracer-ics2017 <tracer@njuics.org>
2020-05-19 20:35:55 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:35:55 up  1:12,  2 users,  load average: 0.27, 0.23, 0.24 1c603f38aaaecaffa7b2743fee30aed52dcd665b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -192,6 +192,7 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   //
+  printf("%d",*result);
   switch (width){
     case 1 :
       t0 = (*result & 0x000000ff);
