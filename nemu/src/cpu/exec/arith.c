d0c483d9337080293c0b55c2803d2d3c520a0d09
tracer-ics2017 <tracer@njuics.org>
2020-05-19 20:32:09 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:32:09 up  1:08,  2 users,  load average: 0.40, 0.24, 0.25 3b35ee1851e1658b1cc17fd7d6b6df14eba63ffe

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -46,7 +46,6 @@ make_EHelper(cmp) {
   //TODO();
   rtl_sub(&t2, &id_dest->val, &id_src->val);
   rtl_slt(&t3, &id_dest->val, &t2);
-  operand_write(id_dest, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
 
