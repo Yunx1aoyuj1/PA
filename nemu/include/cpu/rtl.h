70b42282350d74ab1e4952f36b6c03448319af13
tracer-ics2017 <tracer@njuics.org>
2020-05-19 16:41:31 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:41:31 up  1:42,  2 users,  load average: 0.16, 0.31, 0.35 7a4eb641f26137ec8e69f52b532772049b1ced41

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -199,9 +199,6 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
     case 2 :
       t0 = (*result) & 0x0000ffff;
       break;
-    case 4 :
-      t0 = (*result) & 0xffffffff;
-      break;
   }
   rtl_eq0(&t1,&t0);
   rtl_set_ZF(&t1);
