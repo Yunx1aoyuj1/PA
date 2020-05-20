5a2fda64e2c6f29009b9d8dfd5bcba035a7b8db6
tracer-ics2017 <tracer@njuics.org>
2020-05-20 11:51:34 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:51:34 up 38 min,  2 users,  load average: 0.10, 0.09, 0.09 decdb9ffd5818840638cbe72626125745c468b59

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-
   TODO();
+  //printf("get a interrupt %d at address 0x%x\n",,ret_addr);
 }
 
 void dev_raise_intr() {
