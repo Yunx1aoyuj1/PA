96d93dd3a4476762b8082f7498ad67be3b79c057
tracer-ics2017 <tracer@njuics.org>
2020-05-20 11:00:57 +0800

    >  compile 161820127 liaojunxuan Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:00:57 up 31 min,  2 users,  load average: 0.31, 0.45, 0.40 4ddc4592f5a03c7cdd1551366643015383fb7ab9

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -56,7 +56,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   //rtl_li(&t0,id_src ->val);
-  pio_write(id_dest -> addr , id_dest -> width,id_src ->val);
+  pio_write(id_dest -> val , id_dest -> width,id_src ->val);
 
   print_asm_template2(out);
 
