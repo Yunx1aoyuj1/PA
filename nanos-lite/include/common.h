304af3ea6892065f3b099670fc7d26ad7d040d46
tracer-ics2017 <tracer@njuics.org>
2020-06-06 00:18:58 +0800

    >  run 161820127 liaojunxuan Linux ubuntu 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:18:58 up  1:02,  1 user,  load average: 0.26, 0.18, 0.18 fab884a675cfe4954ad5467345b62e8c72ce1845

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -4,7 +4,7 @@
 #include <am.h>
 #include <klib.h>
 #include "debug.h"
-
+extern _RegSet* do_syscall(_RegSet *r) ;
 typedef char bool;
 #define true 1
 #define false 0
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,9 @@
 #include "common.h"
-
+//#include "syscall.c"
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
-    //case _EVENT_SYSCALL:do_syscall(r);break;
+    case _EVENT_SYSCALL:do_syscall(r);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
