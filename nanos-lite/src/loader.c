e66900744f9a608803dcd580d543a5c96c5080bc
tracer-ics2017 <tracer@njuics.org>
2020-06-05 21:37:34 +0800

    >  run 161820127 liaojunxuan Linux ubuntu 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:37:34 up 27 min,  1 user,  load average: 0.13, 0.19, 0.13 7298363f60a96d24b4e8ce2bac5a43552fa3c5b3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,16 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
+// 把`buf`中的`len`字节写入到ramdisk中`offset`偏移处
+//void ramdisk_write(const void *buf, off_t offset, size_t len);
+
+// 返回ramdisk的大小, 单位为字节
+extern size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
