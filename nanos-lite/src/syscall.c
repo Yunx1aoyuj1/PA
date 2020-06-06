27d103a6720368a7cc6c2c9e1419c88cf93bde96
tracer-ics2017 <tracer@njuics.org>
2020-06-06 10:51:02 +0800

    >  run 161820127 liaojunxuan Linux ubuntu 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:51:02 up 59 min,  1 user,  load average: 0.18, 0.23, 0.24 8865fbaf74731327dfbc84888cbc05c96c0c0ecd

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,11 +9,11 @@ static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t 
   return 1;
 }
 
-static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
+static inline uintptr_t sys_write(_RegSet *r,uintptr_t fd, uintptr_t buf, uintptr_t len) {
   //TODO();
-  
+  SYSCALL_ARG1(r) = fs_write(fd,(void *)buf,len);
   //fs_write(fd,(void *)buf,len);
-  return fs_write(fd,(void *)buf,len);
+  return 1;
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
@@ -61,7 +61,7 @@ _RegSet* do_syscall(_RegSet *r) {
       sys_exit(r);
       break;
     case SYS_write:
-      SYSCALL_ARG1(r) = sys_write(a[1],a[2],a[3]);
+      sys_write(r,a[1],a[2],a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
