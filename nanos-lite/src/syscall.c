d26843f93f28a3fe4ebf7f9f2b1e40909a7d13b1
tracer-ics2017 <tracer@njuics.org>
2020-06-16 00:00:57 +0800

    >  run 161820127 liaojunxuan Linux ubuntu 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:00:57 up  4:29,  1 user,  load average: 0.26, 0.33, 0.27 71e9b21ff6ea381f60cf1dec69dc9cba66f5fd9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,7 +35,7 @@ static inline uintptr_t sys_close(_RegSet *r,uintptr_t fd) {
 
 static inline uintptr_t sys_brk(_RegSet *r) {
   //TODO();
-  SYSCALL_ARG1(r) = 0;
+  SYSCALL_ARG1(r) = mm_brk(SYSCALL_ARG2(r));
   return 1;
 }
 
