882555f4c4df808c257020951d24da9f3832fc7b
tracer-ics2017 <tracer@njuics.org>
2020-06-15 23:46:53 +0800

    >  run 161820127 liaojunxuan Linux ubuntu 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:46:53 up  4:15,  1 user,  load average: 0.19, 0.21, 0.19 91db442eea7f551097ef0bb45b10d1a2ddb088d4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "fs.h"
+#include "memory.h"
 #define DEFAULT_ENTRY ((void *)0x8048000)
 // 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
@@ -9,11 +10,23 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
 // 返回ramdisk的大小, 单位为字节
 extern size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   int fd = fs_open(filename,0,0);
   size_t size = fs_filesz(fd);
-  fs_read(fd,DEFAULT_ENTRY,size);
+  //fs_read(fd,DEFAULT_ENTRY,size);
+  void *va = DEFAULT_ENTRY;
+  void *pa = NULL;
+  int count_of_page = size/ PGSIZE + 1;
+  
+  for(int i = 0 ; i < count_of_page ; i++){
+    pa = new_page();
+    Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+    _map(as, va, pa);
+    fs_read(fd,pa,PGSIZE);
+    va += PGSIZE;
+  }
   fs_close(fd);
   
   return (uintptr_t)DEFAULT_ENTRY;
