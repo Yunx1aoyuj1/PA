6a039a1b7d8b4cdd5c5ab4aabb5cc04ab51f803a
tracer-ics2017 <tracer@njuics.org>
2020-06-19 20:25:08 +0800

    >  run 161820127 liaojunxuan Linux ubuntu 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:25:08 up 38 min,  1 user,  load average: 0.06, 0.09, 0.15 8b330f5d8615e4236477611ba477bab23c7ccde

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,6 +30,6 @@ void load_prog(const char *filename) {
 _RegSet* schedule(_RegSet *prev) {
   current -> tf = prev;
   current = &pcb[0];
-
-  return NULL;
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  return current -> tf;
 }
